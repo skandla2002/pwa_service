653b0ec1962f59b69c65f50c2987dadcfa22ddb7
Andy Pak <skandla2002@gmail.com>
2021-01-15 00:09:21 +0900

    offline에서 호출하기

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # PWA 프로젝트를 생성해 본다.
+참조: https://developers.google.com/web/ilt/pwa/
 
 1. Offline app
---
+++ service-worker.js
@@ -0,0 +1,43 @@
+var CACHE_NAME = 'static-cache';
+var urlsToCache = [
+    'app/',
+    'app/index.html',
+    'app/style'
+];
+
+self.addEventListener('install', function(event){
+    event.waitUntil(
+        cache.open(CACHE_NAME)
+        .then(function(cache){
+            return cache.addAll(urlsToCache);
+        })
+    )
+});
+
+self.addEventListener('fetch', function(event){
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response){
+            return response || fetchAndCache(event.request);
+        })
+    )
+});
+
+
+function fetchAndCache(url){
+    return fetch(url)
+        .then(function(response){
+            if(!response.ok){
+                throw Error(response.statusText);
+            }
+            return caches.open(CACHE_NAME)
+                .then(function(cache){
+                    cache.put(url, response.clone());
+                    return response;
+                })
+        })
+        .then(function(error){
+            console.log('Request fail: ', error);
+        })
+}
+
